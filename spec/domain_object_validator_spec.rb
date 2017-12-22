644fb019d8ec0f0b9356d8814a0b7634eca92545
haazime <h4zime@gmail.com>
2017-12-22 19:21:45 +0900

    method options

--- spec/domain_object_validator_spec.rb
+++ spec/domain_object_validator_spec.rb
@@ -2,13 +2,33 @@ require 'spec_helper'
 require 'capacity_form'
 
 RSpec.describe ActiveModel::Validations::DomainObjectValidator do
-  it do
-    form = CapacityForm.new(work_time_in_minutes: 5 * 60)
-    expect(form).to be_valid
+  context 'construct by new' do
+    it do
+      form = CapacityForm.new(work_time_in_minutes: 5 * 60)
+      expect(form).to be_valid
+    end
+
+    it do
+      form = CapacityForm.new(work_time_in_minutes: 9 * 60)
+      aggregate_failures do
+        expect(form).to_not be_valid
+        expect(form.errors[:work_time_in_minutes]).to match_array(['is invalid'])
+      end
+    end
   end
 
-  it do
-    form = CapacityForm.new(work_time_in_minutes: 9 * 60)
-    expect(form).to_not be_valid
+  context 'construct by from_hours' do
+    it do
+      form = CapacityForm.new(work_time_in_hours: 5)
+      expect(form).to be_valid
+    end
+
+    it do
+      form = CapacityForm.new(work_time_in_hours: 9)
+      aggregate_failures do
+        expect(form).to_not be_valid
+        expect(form.errors[:work_time_in_hours]).to match_array(['is invalid'])
+      end
+    end
   end
 end
--- spec/forms/capacity_form.rb
+++ spec/forms/capacity_form.rb
@@ -3,7 +3,8 @@ require 'work_time'
 class CapacityForm
   include ActiveModel::Model
 
-  attr_accessor :date, :work_time_in_minutes
+  attr_accessor :date, :work_time_in_minutes, :work_time_in_hours
 
-  validates_domain_object_of :work_time_in_minutes, object_class: WorkTime
+  validates_domain_object_of :work_time_in_minutes, object_class: WorkTime, allow_nil: true
+  validates_domain_object_of :work_time_in_hours, object_class: WorkTime, method: :from_hours, allow_nil: true
 end
