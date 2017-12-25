09f19346daf1e3db6935febf1ae53fabb9859d8b
haazime <h4zime@gmail.com>
2017-12-25 19:51:54 +0900

    wip

--- spec/domains/work_time.rb
+++ spec/domains/work_time.rb
@@ -1,10 +1,25 @@
 WorkTime = Struct.new(:minutes) do
-  def self.from_hours(hours)
-    new(hours * 60)
+  class << self
+    attr_accessor :error_thrower
+
+    def from_hours(hours)
+      new(hours * 60)
+    end
+
+    ERROR_THROWERS = {
+      default: -> { raise ArgumentError },
+      custom: -> { raise DomainObjectArgumentError(key: :invalid, scope: [:domain_objects, :work_time]) }
+    }
+
+    def set_error_thrower(key)
+      @error_thrower = ERROR_THROWERS[key]
+    end
   end
 
   def initialize(minutes)
-    raise DomainObjectArgumentError.new(nil, key: :invalid, scope: [:domain_objects, :work_time]) if minutes > 60 * 8
+    self.class.error_thrower.call if minutes > 60 * 8
     super
   end
 end
+
+WorkTime.set_error_thrower(:default)
--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -6,9 +6,13 @@ RSpec.describe ActiveModel::Validations::DomainObjectValidator do
     ValidatesDomainObjectOf.load_i18n_locales
     I18n.locale = :ja
   end
-  after { CapacityForm.clear_validators! }
 
-  it do
+  after do
+    CapacityForm.clear_validators!
+    WorkTime.set_error_thrower(:default)
+  end
+
+  xit do
     CapacityForm.validates_domain_object_of(:work_time_in_hours, object_class: WorkTime, method: :from_hours)
     form = CapacityForm.new(work_time_in_hours: 9)
     form.valid?
